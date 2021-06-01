085340b4a898ea253c94e756d53a8afb9b244634
wargame <or.sekler@campus.technion.ac.il>
2021-06-01 14:34:49 +0300

    new repository

--- src/WarGame.java
+++ src/WarGame.java
@@ -66,21 +66,58 @@ public class WarGame {
             playingCard_2 = secondPlayer.drawCard();
             waitDeck_2.addCard(secondPlayer.drawCard());
             System.out.println(secondPlayer.getName() + " drew " + playingCard_2.toString());
+            // changing the winning deck to the playing deck if the playing deck is empty
+            if (startingPlayer.playingDeck.isEmpty()){
+                startingPlayer.winingDeck.shuffle();
+                startingPlayer.playingDeck = startingPlayer.winingDeck;
+            }
+            if (secondPlayer.playingDeck.isEmpty()){
+                secondPlayer.winingDeck.shuffle();
+                secondPlayer.playingDeck = startingPlayer.winingDeck;
+            }
+            // not a war turn
             // starting player won the turn
             if (playingCard_1.getNumber() > playingCard_2.getNumber()) {
-                startingPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_1);
                 startingPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_2);
+                startingPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_1);
+                System.out.println(startingPlayer.getName() + " won ");
             }
             // second player won the turn
             else if (playingCard_1.getNumber() < playingCard_2.getNumber()) {
-                secondPlayer.addCardToDeck(secondPlayer.winingDeck, waitDeck_1);
-                secondPlayer.addCardToDeck(secondPlayer.winingDeck, waitDeck_2);
+                secondPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_2);
+                secondPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_1);
+                System.out.println(secondPlayer.getName() + " won ");
             }
             // we entered WAR GAME
             else {
+                while (playingCard_1.getNumber() == playingCard_2.getNumber()) {
+                    System.out.println("Starting a war...");
+                    // drawing the three war cards
+                    for (int i = 0; i < 3; i++) {
+                        playingCard_1 = startingPlayer.drawCard();
+                        waitDeck_1.addCard(playingCard_1);
+                        playingCard_2 = secondPlayer.drawCard();
+                        waitDeck_2.addCard(secondPlayer.drawCard());
+                    }
+                    // printing the last war card
+                    System.out.println(startingPlayer.getName() + " drew " + playingCard_1.toString());
+                    System.out.println(secondPlayer.getName() + " drew " + playingCard_2.toString());
 
-
-
+                    // starting player won the turn
+                    if (playingCard_1.getNumber() > playingCard_2.getNumber()) {
+                        for (int j = 0; j < 3; j++){
+                            startingPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_2);
+                            startingPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_1);
+                        }
+                    }
+                    // second player won the turn
+                    else if (playingCard_1.getNumber() < playingCard_2.getNumber()) {
+                        for (int j = 0; j < 3; j++){
+                            secondPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_2);
+                            secondPlayer.addCardToDeck(startingPlayer.winingDeck, waitDeck_1);
+                        }
+                    }
+                }
             }
         }
     }
